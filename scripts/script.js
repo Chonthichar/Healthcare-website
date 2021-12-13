9bc8b92b26806c0206b95d8b1367966873d0f4cb
Chonthichar <chonthichar.soythong@gmail.com>
2021-12-13 23:18:41 +0700

    layout changing

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,17 @@
 //main homepage photos scroll down by using Jquery
-$(document).ready(function () {
-    $("#showcase .btn").on('click', function (event) {
-        if (this.has !== "") {
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
             event.preventDefault();
-            const hash = this.hash;
+            var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, function () {
+            }, 800, function(){
                 window.location.hash = hash;
             });
-        }
+        } // End if
     });
 });
 
